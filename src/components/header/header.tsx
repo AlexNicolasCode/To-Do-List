4f3c8c666bc9d3e7d87d6a7513ea6327d4fa2579
Alex <alexoliveira604@gmail.com>
2021-06-15 19:50:33 -0300

    .

--- src/components/header/header.tsx
+++
@@ -1,9 +0,0 @@
-import style from './styles.module.scss'
-
-export function Header() {
-    return (
-        <header className={style.header}>
-            <h1>To-Do List</h1>
-        </header>
-    )
-}
